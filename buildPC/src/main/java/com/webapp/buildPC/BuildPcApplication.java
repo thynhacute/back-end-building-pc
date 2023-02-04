e68184ceae8f179779a38c8353b2cad8fb816f57
Hoangtammht <hoangtammht@gmail.com>
2023-02-04 17:45:51 +0700

    Create project building PC

---
+++ buildPC/src/main/java/com/webapp/buildPC/BuildPcApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.buildPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildPcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildPcApplication.class, args);
+	}
+
+}
---
+++ buildPC/src/test/java/com/webapp/buildPC/BuildPcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.buildPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildPcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
