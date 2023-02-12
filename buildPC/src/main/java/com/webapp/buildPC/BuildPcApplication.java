1027342604bf0aed22cb992490f8410116057cda
NguyenThinh <koulse.kudaza@gmail.com>
2023-02-12 15:03:25 +0700

    Config and testing JWT

--- buildPC/src/main/java/com/webapp/buildPC/BuildPcApplication.java
+++ buildPC/src/main/java/com/webapp/buildPC/BuildPcApplication.java
@@ -2,12 +2,23 @@ package com.webapp.buildPC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BuildPcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuildPcApplication.class, args);
+
+	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }
---
+++ buildPC/src/main/java/com/webapp/buildPC/jwtConfig/model/JwtRequest.java
@@ -0,0 +1,14 @@
+package com.webapp.buildPC.jwtConfig.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtRequest {
+
+    private String username;
+    private String password;
+}
