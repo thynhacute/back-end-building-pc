dd07a76cfb00a56f0492e539ad04a824ef5de1c8
NguyenThinh <koulse.kudaza@gmail.com>
2023-02-23 14:34:52 +0700

    Update API google token

--- buildPC/src/main/java/com/webapp/buildPC/filter/CustomAuthorizationFilter.java
+++ buildPC/src/main/java/com/webapp/buildPC/filter/CustomAuthorizationFilter.java
@@ -31,7 +31,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (request.getServletPath().equals("/login") || request.getServletPath().equals("/user/token/refresh") || request.getServletPath().equals("/user/token/google")) {
+        if (request.getServletPath().equals("/login") || request.getServletPath().equals("/user/token/refresh") || request.getServletPath().equals("/user/token/google") || request.getServletPath().equals("/user/login/user")) {
             filterChain.doFilter(request, response);
         } else {
             String authorizationHeader = request.getHeader("Authorization");
--- buildPC/src/main/java/com/webapp/buildPC/swagger/configSwagger.java
+++ buildPC/src/main/java/com/webapp/buildPC/swagger/configSwagger.java
@@ -2,22 +2,34 @@ package com.webapp.buildPC.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class configSwagger {
     @Bean
-    public Docket postsApi(){
-        return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.webapp.buildPC.controller"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build();
     }
     private ApiInfo apiInfo(){
@@ -25,5 +37,17 @@ public class configSwagger {
                 .description("Website for building and selling pc")
                 .termsOfServiceUrl("")
                 .licenseUrl("thinhknc@gmail.com").version("1.0").build();
+    }
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 }
