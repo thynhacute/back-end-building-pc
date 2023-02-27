20e954b663c1e8c85c8edcfd286d690b64b1d753
Hoangtammht <hoangtammht@gmail.com>
2023-02-27 16:28:07 +0700

    Add component function

--- buildPC/src/main/java/com/webapp/buildPC/controller/ComponentController.java
+++ buildPC/src/main/java/com/webapp/buildPC/controller/ComponentController.java
@@ -1,11 +1,16 @@
 package com.webapp.buildPC.controller;
 
 import com.webapp.buildPC.domain.Component;
+import com.webapp.buildPC.domain.Transaction.AddComponentRequest;
 import com.webapp.buildPC.service.interf.ComponentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @CrossOrigin
@@ -22,18 +27,22 @@ public class ComponentController {
     }
 
     @GetMapping("/componentByName")
-    public ResponseEntity<List<Component>> getComponentByName(@RequestBody String componentName){
+    public ResponseEntity<List<Component>> getComponentByName(@RequestParam String componentName){
         return ResponseEntity.ok().body(componentService.getComponentByName(componentName));
     }
 
     @GetMapping("/componentByBrand")
-    public ResponseEntity<List<Component>> getComponentByBrand(@RequestBody String brandID){
+    public ResponseEntity<List<Component>> getComponentByBrand(@RequestParam String brandID){
         return ResponseEntity.ok().body(componentService.getComponentByBrand(brandID));
     }
 
     @PostMapping("/addComponent")
-    public String addComponent() {
-        return "The page for admin";
+    public ResponseEntity<Component> addComponent(@RequestParam String componentName, @RequestParam("image")  MultipartFile image,
+                                                      @RequestParam int price, @RequestParam int amount,@RequestParam String description,
+                                                  @RequestParam int brandID, @RequestParam String categoryID) throws IOException {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/component/addComponent")
+                .toUriString());
+        return ResponseEntity.created(uri).body(componentService.addComponent(componentName, amount ,price, image, description, brandID, categoryID));
     }
 
     @PostMapping("/buyComponent")
